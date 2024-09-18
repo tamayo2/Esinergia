bc87e0ee6be44849bbce0c76d9fbab41ebee22b1
tamayo2 <juan.tamayo3@utp.edu.co>
2024-09-18 16:19:48 -0500

    Fork

--- components/atoms/BasicCard.stories.ts
+++ components/atoms/BasicCard.stories.ts
@@ -16,7 +16,8 @@ const defaultCard = {
  *  */ 
 
 export default {
-  title: 'Molecules/Cards',
+  title: 'Atoms/Cards',
+  tags: ['autodocs'],
   componente: Card,
   argTypes: {
     title: {
@@ -46,8 +47,16 @@ export const secondary = ({
   title,
   }) => Card({
   title
-  })
+})
 
+Alessandro - CEO (backend)
+Gerente - Juan (Cultural) (NUNCA!) ------> Exitos proyectos, certificados de inglés (Institución) A2, Experiencia, Certtificados Drupal, Sistemas
+
+Jorge - junior (Semisenior - Senior) --- Drupal 
+David - junior 
+Begginer  estimaciones ( 1h ) , 2h levantar la mano (a través)
+
+feat:
 
 /* 
   React.js o Next.js
@@ -57,6 +66,11 @@ export const secondary = ({
   4- Van crear los archivos .stories.js/ts en el mismo  lugar del componente de cada componente
   5- Comenzar documentación base: Controles configurados, ojalé notas notas y variaciones del componente
 
+
+
   nota: crear .nvmrc
+  Nota hiper importante: Lo que no está documentado no existe
+
+  axios, validar, zod - valibot (typeguards?) ------> import { z } from 'z', import { number, string, boolean } from 'valibot'
 
 */
